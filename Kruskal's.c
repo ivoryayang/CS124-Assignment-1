7f815afa48d04aea1d05ea0679d3be592dc0f7e1
ivoryarayang <92782295+ivoryarayang@users.noreply.github.com>
2022-02-27 09:34:41 -0800

    Update Kruskal's.c

--- Kruskal's.c
+++ Kruskal's.c
@@ -1,3 +1,12 @@
-int generateMST(int n, int d, int edges, edge *graph[n * (n - 1) / 2], edge *mst[n - 1]) {
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <time.h>
+#include <math.h>
+#include "graph.h"
+#include "disjoint.h"
+
+// Generate a minimum spanning tree 
+int generateMST(int v, int d, int edges, edge *graph[n * (n - 1) / 2], edge *mst[n - 1]) {
     // Sorts edges by weight
     mergeSort(graph, 0, edges - 1);
