c1d4698477a7178b8d2df5ab03f1a312b4a05542
ivoryarayang <92782295+ivoryarayang@users.noreply.github.com>
2022-02-27 03:54:54 -0800

    Update graph.h

--- graph.h
+++ graph.h
@@ -4,21 +4,4 @@ typedef struct {
 	float length;
 } edge;
 
-// Generates a graph with n vertices in adjacency matrix form
-// Input: 	n - number of vertices
-// 			d - dimension of vertex. if 0, then random edge lengths
-//			graph - pointer to edge array containing all edges in graph
-// Output: returns number of edges created; generated graph stored in graph
-int generateGraph(int n, int d, int *cap, edge ***graph);
-
-// Frees all memory allocated in graph
-// Input:   edges - number of edges
-//          graph - array of (n C 2) edges
-void destroyGraph(int edges, edge* graph[]);
-
-// Pretty prints the graph to stdout for testing
-// Prints in matrix form (bottom triangular)
-// NOTE: should not be used for large values of n!
-// Input:   edges - number of edges
-//          graph - array of (n C 2) edges
-void printGraph(int n, edge* graph[]);
+int generate_graph(int n, int d, int *cap, edge ***graph);
