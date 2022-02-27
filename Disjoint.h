e5288997008350748569373ce57820f3081001f0
ivoryarayang <92782295+ivoryarayang@users.noreply.github.com>
2022-02-27 03:01:07 -0800

    Update Disjoint.h

--- Disjoint.h
+++ Disjoint.h
@@ -1,1 +1,10 @@
+typedef struct node node;
 
+// Create new node with value x
+node *makeset(int x);
+
+// Find root of tree that n is in
+node *find(node *n);
+
+// Join trees containing node x and node y
+void join(node *x, node *y);
