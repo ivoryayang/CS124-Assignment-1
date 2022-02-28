00ad18803d6704c1be84ced63fc3eceb750cf526
ivoryarayang <92782295+ivoryarayang@users.noreply.github.com>
2022-02-27 19:50:42 -0800

    Update graph.h

--- graph.h
+++ graph.h
@@ -1,7 +1,8 @@
+#pragma once
 typedef struct {
 	int p1;
 	int p2;
 	float length;
 } edge;
 
-int generate_graph(int n, int d, int *cap, edge ***graph);
+int generate_graph(int n, int d, edge** graph,int sample_size);
