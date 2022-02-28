280fae4e19b8951a94cd319b334990e00f4b5ecb
ivoryarayang <92782295+ivoryarayang@users.noreply.github.com>
2022-02-27 19:53:30 -0800

    Update Disjoint.h

--- Disjoint.h
+++ Disjoint.h
@@ -1,10 +1,6 @@
-typedef struct node node;
+#pragma once
 
-// Create new node with value x
-node *makeset(int x);
-
-// Find root of tree that n is in
-node *find(node *n);
-
-// Join trees containing node x and node y
-void join(node *x, node *y);
+void set_dsu(int* parent, int* rank, int n);
+int find(int a, int* parent);
+int set_count(int* parent, int n);
+void merge_set(int a, int b, int* parent, int* rank);
